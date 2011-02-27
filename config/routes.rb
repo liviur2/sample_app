1891b13ebe688e6d4aef05f34675415c5eadf747
Liviu Radoi <keller946@yahoo.com>
2011-02-28 00:27:02 +0200

    finished first cut @ UserModel (6.4)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
   # get "pages/contact"
   # get "pages/about"
   # get "pages/help"
+  resources :users
   match '/signup', :to =>'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
